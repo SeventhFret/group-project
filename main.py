152e23ee2a46dc3548ded871e544c16dcf59bdde
semen_zamai <zamay.sv@gmail.com>
2023-01-27 15:02:06 +0100

    leap year

---
+++ main.py
@@ -0,0 +1,6 @@
+import datetime # already done
+year = 2023     # already done
+if (year % 4 == 0):
+    print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
